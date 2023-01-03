a249a692abb1f7217ee3c2b714f202582e68b058
Guoxin <lgx31@sina.cn>
2023-01-03 10:52:21 +0800

    fix: bin scripts --bug=0

--- signle-org/node3/bin/profloop.sh
+++ signle-org/node3/bin/profloop.sh
@@ -3,24 +3,24 @@
 IP=$1
 MONITOR_PORT=$2
 PPROF_PORT=$2
-PATH=$2
+OUT_PATH=$2
 
 if [ -z "$ip" ]; then
     echo "please specify ip:port: [usage] heap.sh ip:port"
     exit
 fi
 
-if [ -z "${path}" ]; then
-    path=/tmp/prof
+if [ -z "${OUT_PATH}" ]; then
+    OUT_PATH=/tmp/prof
 fi
-mkdir -p $path
+mkdir -p OUT_PATH
 while :
 do
     FILE_NO=$(date "+%Y-%m-%d_%H:%M:%S")
     echo "$FILE_NO"
-    curl -sK -v  http://"${IP}:${PPROF_PORT}"/debug/pprof/heap > ${path}/heap"${FILE_NO}".prof
-    curl -sK -v  http://"${IP}:${PPROF_PORT}"/debug/pprof/goroutine > ${path}/goroutine"${FILE_NO}".prof
-    curl -sK -v  http://"${IP}:${PPROF_PORT}"/debug/pprof/profile > ${path}/profile"${FILE_NO}".prof
-    curl -sK -v  http://"${IP}:${MONITOR_PORT}"/debug/pprof/metrics > ${path}/metrics"${FILE_NO}".list
+    curl -sK -v  http://"${IP}:${PPROF_PORT}"/debug/pprof/heap > ${OUT_PATH}/heap"${FILE_NO}".prof
+    curl -sK -v  http://"${IP}:${PPROF_PORT}"/debug/pprof/goroutine > ${OUT_PATH}/goroutine"${FILE_NO}".prof
+    curl -sK -v  http://"${IP}:${PPROF_PORT}"/debug/pprof/profile > ${OUT_PATH}/profile"${FILE_NO}".prof
+    curl -sK -v  http://"${IP}:${MONITOR_PORT}"/debug/pprof/metrics > ${OUT_PATH}/metrics"${FILE_NO}".list
     sleep 120
 done
