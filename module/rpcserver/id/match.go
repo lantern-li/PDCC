a28ba71fdb764769fc4a540111d403eb77c63245
taifu <515789026@qq.com>
2024-06-28 21:35:46 +0800

    refactor: Match subscriberId participantId logic --story=0

--- module/rpcserver/id/match.go
+++ module/rpcserver/id/match.go
@@ -1,18 +1,15 @@
 /*
-   Created by guoxin in 2022/11/17 9:24 AM
+Created by guoxin in 2022/11/17 9:24 AM
 */
 package id
 
 import (
 	"chainmaker.org/chainmaker/protocol/v2"
 	"fmt"
+	"strings"
 )
 
 var IdentityMatchInstance = IdentityMatchImpl{}
-
-type IdentityMatch interface {
-	Match(subscriberId string, participantId ParticipantId) bool
-}
 
 type IdentityMatchImpl struct {
 	log protocol.Logger
@@ -21,20 +18,38 @@ type IdentityMatchImpl struct {
 func InitIdentityMatch(log protocol.Logger) {
 	IdentityMatchInstance = IdentityMatchImpl{log: log}
 }
-func (m IdentityMatchImpl) Match(subscriberId SubscriberId, participantId ParticipantId) bool {
-	subscriberSubId, offset := subscriberId.GetIdentity()
-	participantSubId := participantId.GetIdentity(subscriberId.GetType(), offset)
-	if subscriberSubId == participantSubId {
-		m.log.DebugDynamic(func() string {
-			return fmt.Sprintf("identity match, subscriberId:%v,participantId:%v,subscriberSubId:%v,offset:%v,"+
-				"participantSubId:%v", subscriberId.GetId(), participantId, subscriberSubId, offset, participantSubId)
-		})
+
+/*
+Match 用于匹配订阅者和参与者的身份
+subscriberId: 订阅者身份
+participantId: 参与方ID，取自链上交易
+
+2 01 02 03 04 0000000000
+
+1 10 02 03 04 0000000000
+*/
+func (m IdentityMatchImpl) Match(subscriberId SubscriberId, participantId string) bool {
+	sType := subscriberId.GetType()
+	switch sType {
+	case TaxationAdministrationCode:
 		return true
-	} else {
-		m.log.DebugDynamic(func() string {
-			return fmt.Sprintf("identity mismatching, subscriberId:%v,participantId:%v,subscriberSubId:%v,offset:%v,"+
-				"participantSubId:%v", subscriberId.GetId(), participantId, subscriberSubId, offset, participantSubId)
-		})
-		return false
+	case GovernmentBodyCode, SocialCreditCodeCode, IDCardNumberCode:
+		return subscriberId.GetId() == participantId
+	case TaxationCode: // 省局
+		switch participantId[:1] {
+		case TaxationAdministrationCode: // 总局
+			return false
+		case TaxationCode, IDCardNumberCode: // 省局 个人
+			identity, _ := subscriberId.GetIdentity()                              // 取出省市区
+			return strings.HasPrefix(participantId[ValidTaxAndIdIndex:], identity) // 前缀匹配
+		case GovernmentBodyCode, SocialCreditCodeCode: // 外部政府 企业
+			identity, _ := subscriberId.GetIdentity()
+			return strings.HasPrefix(participantId[ValidGovAndEnpIndex:], identity)
+		}
 	}
+	m.log.DebugDynamic(func() string {
+		return fmt.Sprintf("identity mismatching, subscriberId:%v, participantId:%v", subscriberId.GetId(), participantId)
+	})
+	return false
+
 }
--- module/rpcserver/id/subscriber.go
+++ module/rpcserver/id/subscriber.go
@@ -1,5 +1,7 @@
 /*
-Created by guoxin in 2022/11/17 9:34 AM
+Copyright (C) BABEC. All rights reserved.
+
+SPDX-License-Identifier: Apache-2.0
 */
 package id
 
@@ -20,33 +22,58 @@ type SubscriberIdImpl struct {
 	offset   int
 }
 
+/*
+	NewSubscriberId
+
+总 		0 00 01 00 0000 X 不存在总局有区的情况
+总 		0 00 00 00 0000 √
+省	  	1 43 00 00 0000 √ -> 1 43
+省市  	1 43 01 00 0000 √ -> 1 43 01
+省市区 	1 43 01 11 0000 √ -> 1 43 01 11
+其他（2 3 4）均全匹配
+*/
 func NewSubscriberId(id string) (SubscriberId, error) {
-	subscriberId := &SubscriberIdImpl{
-		Id:  id,
-		typ: string(id[0]),
+	if len(id) <= 2 {
+		return nil, errors.New("subscriberId identifier is invalid, identifier length is less than 2")
 	}
 
-	if subscriberId.typ == SocialCreditCodeCode || subscriberId.typ == GovernmentBodyCode || subscriberId.typ == TaxationAdministrationCode {
+	subscriberId := &SubscriberIdImpl{
+		Id:  id,
+		typ: id[:1],
+	}
+
+	// 全匹配： 国家税务总局 0 外部政府机构 2 企业 3 自然人 4
+	if subscriberId.typ == TaxationAdministrationCode ||
+		subscriberId.typ == GovernmentBodyCode ||
+		subscriberId.typ == SocialCreditCodeCode ||
+		subscriberId.typ == IDCardNumberCode {
 		subscriberId.identity = id
 		subscriberId.offset = 0
 		return subscriberId, nil
 	}
+
+	// 省市区匹配 00，找出对应的前缀
 	var index int
-	for i := 1; i < len(id); i += 2 {
-		if id[i:i+2] == Both {
+	for i := 1; i < len(id)-2; i += 2 {
+		if id[i:i+2] == DoubleZero {
 			index = i
 			break
 		}
 	}
-	if index == 0 {
-		return nil, errors.New("subscriberId identifier is invalid, identifier does not contain \"00\"")
+
+	// 如果未匹配到00，则代表全有，则为 省市区情况
+	// 如果 index > 7 ，则说明00在后四保留位，则为 省市区情况
+	if index == 0 || index > 7 {
+		index = 7
 	}
 
 	if index%2 == 0 {
 		return nil, errors.New("subscriberId identifier is invalid, the identifier format is incorrect")
 	}
-	subscriberId.identity = id[ValidTaxationCodeIndex:index]
-	subscriberId.offset = index - ValidTaxationCodeIndex
+
+	subscriberId.identity = id[ValidTaxAndIdIndex:index]
+	subscriberId.offset = index - ValidTaxAndIdIndex
+
 	return subscriberId, nil
 }
 
