a31a4df7d12406c50e78cf816f3ffe3197e2ff2f
Guoxin.Liu <lgx31@sina.cn>
2024-07-05 20:22:35 +0800

    fix: v2.3.1 -> v2.3.1.1 --story=0

--- module/blockchain/global.go
+++ module/blockchain/global.go
@@ -9,7 +9,7 @@ package blockchain
 
 var (
 	//CurrentVersion current version
-	CurrentVersion = "v2.3.1"
+	CurrentVersion = "v2.3.1.1"
 	// CurrentCommit current git commit hash
 	GitCommit = ""
 	// CurrentBranch current git branch
