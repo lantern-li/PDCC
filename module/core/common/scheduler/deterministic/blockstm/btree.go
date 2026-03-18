80c59cc62a420ccaccea816dc63598ea5b40744b
denglongli <li-dl24@mails.tsinghua.edu.cn>
2026-03-18 13:43:40 +0800

    feat: add btree.go --story=0

---
+++ module/core/common/scheduler/deterministic/blockstm/btree.go
@@ -0,0 +1,89 @@
+package blockstm
+
+import (
+	"github.com/tidwall/btree"
+	"sync/atomic"
+)
+
+type BTree[T any] struct {
+	atomic.Pointer[btree.BTreeG[T]]
+}
+
+// NewBTree returns a new BTree.
+func NewBTree[T any](less func(a, b T) bool, degree int) *BTree[T] {
+	tree := btree.NewBTreeGOptions(less, btree.Options{
+		NoLocks:  true,
+		ReadOnly: true,
+		Degree:   degree,
+	})
+	t := &BTree[T]{} // 把刚创建好的底层 *btree.BTreeG[T] 指针，放进包装结构BTree[T] 的原子指针里，完成初始化。
+	t.Store(tree)
+	return t
+}
+
+func (bt *BTree[T]) Get(item T) (result T, ok bool) {
+	return bt.Load().Get(item)
+}
+
+func (bt *BTree[T]) GetOrDefault(item T, fillDefaults func(*T)) T {
+	for {
+		t := bt.Load()
+		result, ok := t.Get(item)
+		if ok {
+			return result
+		}
+		fillDefaults(&item)
+		c := t.Copy()
+		c.Set(item)
+		c.Freeze()
+		if bt.CompareAndSwap(t, c) {
+			return item
+		}
+	}
+}
+
+func (bt *BTree[T]) Set(item T) (prev T, ok bool) {
+	for {
+		t := bt.Load()
+		c := t.Copy()
+		prev, ok = c.Set(item)
+		c.Freeze()
+		if bt.CompareAndSwap(t, c) {
+			return
+		}
+	}
+}
+
+func (bt *BTree[T]) Delete(item T) (prev T, ok bool) {
+	for {
+		t := bt.Load()
+		c := t.Copy()
+		prev, ok = c.Delete(item)
+		c.Freeze()
+		if bt.CompareAndSwap(t, c) {
+			return
+		}
+	}
+}
+
+func (bt *BTree[T]) Scan(iter func(item T) bool) {
+	bt.Load().Scan(iter)
+}
+
+func (bt *BTree[T]) Max() (T, bool) {
+	return bt.Load().Max()
+}
+
+func (bt *BTree[T]) Iter() btree.IterG[T] {
+	return bt.Load().Iter()
+}
+
+// ReverseSeek returns the first item that is less than or equal to the pivot
+func (bt *BTree[T]) ReverseSeek(pivot T) (result T, ok bool) {
+	bt.Load().Descend(pivot, func(item T) bool {
+		result = item
+		ok = true
+		return false
+	})
+	return
+}
